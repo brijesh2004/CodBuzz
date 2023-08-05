7e216a7c3bc1a5c130b19c6caf80e85b8aa8d3a1
brijesh2004 <brskumar0102@gmail.com>
2023-08-05 15:02:51 +0530

    responsive

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,8 +16,8 @@ export default function RootLayout({ children }) {
     <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossOrigin="anonymous"></link>
     </head>  
-    {/* <body className={inter.className}> */}
-      <body >
+    <body className={inter.className}>
+      {/* <body > */}
       <div className='bg-dark'>
       <Navbar/>
       {children}
