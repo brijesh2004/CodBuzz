23f0b6b10d0d1e53376880b902961a34385f2126
brijesh2004 <brskumar0102@gmail.com>
2023-08-05 15:16:14 +0530

    responsiveness

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
 import Navbar from './Navbar'
 import './globals.css'
-// import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import Bottom from './Bottom'
 
-// const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'CodBuzz',
@@ -16,14 +16,12 @@ export default function RootLayout({ children }) {
     <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossOrigin="anonymous"></link>
     </head>  
-    <body className={inter.className}>
-      {/* <body > */}
-      <div className='bg-dark'>
+    {/* <body className={inter.className}> */}
+      <body className='bg-dark'>
       <Navbar/>
       {children}
       <Bottom/>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"  async></script>
-      </div>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossOrigin="anonymous"  async></script>
       </body>
     </html>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 "use client"
 import { useState, useEffect } from "react";
-import about from '@/style/about.module.css';
-import contact from '@/style/contact.module.css';
+import about from "@/style/about.module.css";
+import contact from "@/style/contact.module.css";
 import Image from "next/image";
-import Web_developer from '@/Images/web_development.gif';
-import App_developer from '@/Images/app_development.gif';
-import home from '@/style/home.module.css';
+import Web_developer from "@/Images/web_development.gif";
+import App_developer from "@/Images/app_development.gif";
+import home from "@/style/home.module.css";
 import Animation from "./Animation";
 
 import { useRef } from "react";
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <div className="bg-dark text-white">
       <FadeInSection>
-        <div className='main_div'>
+        <div className="main_div">
           <br /><br /><br />
           <h1>Take Your Career to the Next Level with CodeBuzz</h1><br />
           <h3>Join Our Internship Program</h3><br /><br />
@@ -40,7 +40,7 @@ export default function Home() {
       </FadeInSection>
       <FadeInSection>
         <div className="who_we_are_main">
-          <div className='who_we_are'>
+          <div className="who_we_are">
             <h1>Who We Are</h1>
             <p className={about.paragraph}>Welcome to CodBuzz a top Company to offer Internship in many Field of technology.
               We provide internship in Web Developer (frontend , backend and full Stack ) , App Developer , Machine Learning , Data science
@@ -56,7 +56,7 @@ export default function Home() {
 
       <div >
         <center> <h1>Service for the Client</h1></center> <br /><br />
-       <Animation><center>  <h2 style={{ textDecoration: 'underline' }}>Web Development</h2></center><br /><br /><br /></Animation> 
+       <Animation><center>  <h2 style={{ textDecoration: "underline" }}>Web Development</h2></center><br /><br /><br /></Animation> 
         <div className={home.web_developer}>
           <div className={home.web_right_part}>
             <FadeInSection>
@@ -68,7 +68,7 @@ export default function Home() {
               <p>Are You Want a website for your business</p>
               <p>In the Present era every things gone online . </p>
               <p>do you also want to grow your business Online</p>
-              <p>Let's contact with us and grow your business Online</p>
+              <p>Let&apos;s contact with us and grow your business Online</p>
               <button className={contact.submit_btn1}>Contact Us</button>
             </FadeInSection>
           </div>
@@ -81,7 +81,7 @@ export default function Home() {
               <p>Are You Want an App for your business</p>
               <p>In the Present era every things gone online . </p>
               <p>do you also want to grow your business Online</p>
-              <p>Let's contact with us and grow your business Online</p>
+              <p>Let&apos;s contact with us and grow your business Online</p>
               <button className={contact.submit_btn1}>Contact Us</button>
               </FadeInSection>
           </div>
